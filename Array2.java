107cf86b39fe770c5af91cbb9b113aa6760f9c8a
Anupam Chaturvedi <anupamc693@gmail.com>
2025-11-10 16:06:30 +0530

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -1,12 +1,13 @@
-import java.util.*;
+import java.util.*; // Importing the Scanner class for taking user input
 
 public class Array2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Creating a Scanner object to read input from the user
         int[] arr = new int[10];
-        System.out.print("Enter the elements of the array: ");
+        System.out.print("Enter the elements of the array: ");  // Askig user to enter the number(elements)
+        // loop to check the elements
         for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = sc.nextInt();     // input the elemnts in aaray
         }
         int sum = 0;
         for (int i = 0; i < 10; i++) {
